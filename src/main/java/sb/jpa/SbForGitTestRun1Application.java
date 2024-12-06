5fda7e7713b1853239cf481560748baa28f2166b
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2024-12-06 14:05:22 +0530

    Initial Commit.

---
+++ src/main/java/sb/jpa/SbForGitTestRun1Application.java
@@ -0,0 +1,13 @@
+package sb.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbForGitTestRun1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbForGitTestRun1Application.class, args);
+	}
+
+}
