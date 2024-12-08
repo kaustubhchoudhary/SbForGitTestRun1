1a35b2f84bac907b2677d6a0a58468e567efca99
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2024-12-08 18:56:06 +0530

    Completed getting single user functionality

--- src/main/java/sb/jpa/controller/UserController.java
+++ src/main/java/sb/jpa/controller/UserController.java
@@ -1,16 +1,19 @@
 package sb.jpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import sb.jpa.entities.User;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	
 	@GetMapping("/{id}")
-	public String getData() {
-		return "data";
+	public User getData(@PathVariable int id) {
+		return new User(id, "Umesh");
 	}
 	
 	@GetMapping("/")
