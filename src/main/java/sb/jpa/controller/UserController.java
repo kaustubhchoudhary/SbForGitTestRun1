1c897918dede507cb25a0b9d866717c9f3934883
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2024-12-08 18:44:05 +0530

    Completed get all users GET functionality

--- src/main/java/sb/jpa/controller/UserController.java
+++ src/main/java/sb/jpa/controller/UserController.java
@@ -1,24 +1,25 @@
 package sb.jpa.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import sb.jpa.entities.User;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@GetMapping("/{id}")
 	public String getData() {
 		return "data";
 	}
-	
+
 	@GetMapping("/")
-	public String getAll() {
-		System.out.println("New changes 1");
-		System.out.println("New changes 2");
-		System.out.println("New changes 3");
-		return "All";
+	public List<User> getAll() {
+		return List.of(new User(101, "Raj"), new User(102, "Raj"));
 	}
 
 }
